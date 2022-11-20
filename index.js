775d9562658588cd24021ff1f927890921452611
ArunSharmaLearning <arunlivingfantastico@gmail.com>
2022-11-21 00:37:27 +0530

    adding public hostname

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const host = "http://localhost:3000/";
+const host = "https://dropithost.onrender.com/";
 const uploadURL = `${host}api/files`;
 const emailURL = `${host}api/files/send`;
 
